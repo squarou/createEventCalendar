f5165dd71e71f542015ee3614e69d2bdd7fff531
Benjamin <squarou@users.noreply.github.com>
2017-08-03 13:41:40 +0200

    add toPlaceholder and resource ID properties

--- createEventCalendar.php
+++ createEventCalendar.php
@@ -1,6 +1,8 @@
 <?php
 /*
 Params
+    &resource: the id of the resource targeted ; default to current
+    &toPlaceholder: theplaceholder you want to use for the output ; default to calendarLink
     &filePath: Path where the calendar file will be saved, defaults to assets/events directory (define path like /example/)
     &fileName: defaults to pagetitle
     &tpl: name of chunk to use. Defaults to eventCalendar
@@ -16,10 +18,23 @@ Params
 	use the placeholder "calendarLink" to output the download link to the calendar file
 */
 
+$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, 'calendarLink', true);
+
+$resId  = $modx->getOption('resource', $scriptProperties, $modx->resource->get('id'), true);
+
+$here = $modx->getObject('modResource', $resId);
+
+$startDate = $here->getTVValue('DateStart');;
+$endDate = $here->getTVValue('DateEnd');
+$address = $here->getTVValue('adresse');
+
+
 $eventsDir 				= $filePath ?  $_SERVER['DOCUMENT_ROOT'] . $filePath : $modx->getOption('assets_path').'events/';
 $tpl                    = $tpl ? $tpl : 'eventCalendar';
-$eventID                = $modx->resource->get('id');
-$eventName 				= $modx->resource->get('longtitle') ? $modx->resource->get('longtitle') : $modx->resource->get('pagetitle');
+$eventID                = $resId;
+$eventName 				= $here->get('longtitle') ? $here->get('longtitle') : $here->get('pagetitle');
+
+$summary = $here->get('introtext');
 $eventSummary 			= wordwrap($summary, 50, "\n ", true);
 
 $eventStartDate         = strtotime($startDate);
@@ -28,52 +43,34 @@ $eventEndDate           = strtotime($endDate);
 $prodid 				= mt_rand(1000000000, 9999999999) . $eventID;
 
 $addressArray           = explode(",", $address);
-$street                 = trim($addressArray[0]);
-$housenumber            = trim($addressArray[1]);
+
+$housenumber            = trim($addressArray[0]);
+$street                 = trim($addressArray[1]);
 $zipcode                = trim($addressArray[2]);
 $city                   = trim($addressArray[3]);
-$country                = trim($addressArray[4]);
 
-$eventLocation 			= $street . ' ' . $housenumber . ',' . $zipcode . ' ' . $city . ';' . $country;
+$eventLocation 			= $street . ' ' . $housenumber . ',' . $zipcode . ' ' . $city;
+
+$eventLocation = $address;
 
 $geocode = ($geocode == 0) ? 0 : 1;
 //check if a file with this name already exists
-$file = $fileName ? $fileName . ".ics" : $modx->resource->get('pagetitle') . ".ics";
+$file = $fileName ? $resId . "-" . $fileName . ".ics" : $resId . "-" . $here->get('pagetitle') . ".ics";
 $eventsFile = fopen($eventsDir . $file, "w");
 
-if(!$coordinates && $geocode == 1){
 
-	$reverseGeocodeUrl = strip_tags('https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($eventLocation));
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $reverseGeocodeUrl);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-	$response = json_decode(curl_exec($ch), true);
-	
-	if ($response['status'] != 'OK') {
-		//failed
-		continue;
-	}
-
-	$geoCodeResults = array(
-		'street' 		=> $response['results'][0]['address_components'][1]['long_name'],
-		'housenumber'	=> $response['results'][0]['address_components'][0]['long_name'],
-		'zipcode'		=> $response['results'][0]['address_components'][6]['long_name'],
-		'city'			=> $response['results'][0]['address_components'][2]['long_name'],
-		'state'			=> $response['results'][0]['address_components'][4]['long_name'],
-		'country'		=> $response['results'][0]['address_components'][5]['long_name'],
-		'latitude'		=> str_replace(",", ".", $response['results'][0]['geometry']['location']['lat']),
-		'longitude'		=> str_replace(",",".", $response['results'][0]['geometry']['location']['lng'])
-	);
-	
-	$coordinates        = $geoCodeResults['latitude'].','.$geoCodeResults['longitude'];
-}
 
 $eventLocation 			= str_replace(",", "\\n", $eventLocation);
 $eventLocation 			= str_replace(";", "\\n", $eventLocation);
 
+
+
+
+$link = $modx->makeUrl($resId,'','','https');
+
+
 function dateToCal($timestamp) {
-  return date('Ymd\THis\Z', $timestamp);
+  return date('Ymd\THis', $timestamp);
 }
 
 function escapeString($string) {
@@ -129,4 +126,4 @@ $eventsFile = fopen($eventsDir . $file, "w");
 fwrite($eventsFile, $vCalendar);
 fclose($eventsFile);
 
-$modx->toPlaceholder('calendarLink', $filePath . $file,'');
+$modx->toPlaceholder($toPlaceholder, $filePath . $file,'');
